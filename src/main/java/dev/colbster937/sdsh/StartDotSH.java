b3ba4ca5e8f06b7657dfd6063c8d8e4401a3a943
Colbster937 <colbster937@colbster937.dev>
2026-05-28 16:24:08 -0500

    feat: init

---
+++ src/main/java/dev/colbster937/sdsh/StartDotSH.java
@@ -0,0 +1,45 @@
+package dev.colbster937.sdsh;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class StartDotSH {
+  private static final Path EXEC = Paths.get(
+    "start." + (
+      !System.getProperty("os.name").toLowerCase().contains("win")
+        ? "sh"
+        : "exe"
+    )
+  ).toAbsolutePath().normalize();
+
+  public static final void main(final String[] args) throws IOException, InterruptedException {
+    if (Files.exists(EXEC)) {
+      EXEC.toFile().setExecutable(true);
+
+      final ProcessBuilder pb = new ProcessBuilder(EXEC.toString());
+
+      pb.directory(Paths.get("").toAbsolutePath().toFile());
+      pb.inheritIO();
+
+      pb.environment().put(
+        "SDSH_JVM_ARGS",
+        String.join(" ", ManagementFactory.getRuntimeMXBean().getInputArguments())
+      );
+
+      pb.environment().put(
+        "SDSH_PROGRAM_ARGS",
+        String.join(" ", args)
+      );
+
+      final Process proc = pb.start();
+
+      System.exit(proc.waitFor());
+    } else {
+      throw new FileNotFoundException(EXEC.toString());
+    }
+  }
+}
